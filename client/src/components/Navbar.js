d15e60acfe005ebcdb60f05dd23028f40d1630b6
Akash <akashanand3615@gmail.com>
2026-05-22 14:56:33 +0530

    Fixed homepage typo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
+      
+      {/* Logo */}
       <div className="logo-container">
-       <img src="/logo.png" alt="Logo" className="logo-img" />
+        <img src="/logo.png" alt="Logo" className="logo-img" />
         <span className="logo-text">Rishabam Production</span>
       </div>
 
-      <ul className="nav-links">
-        <li><a href="#hero">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#movies">Movies</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact</a></li>
+      {/* Hamburger Button */}
+      <div
+        className="menu-btn"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </div>
+
+      {/* Navigation Links */}
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+        <li>
+          <a href="#hero" onClick={() => setMenuOpen(false)}>
+            Home
+          </a>
+        </li>
+
+        <li>
+          <a href="#about" onClick={() => setMenuOpen(false)}>
+            About
+          </a>
+        </li>
+
+        <li>
+          <a href="#movies" onClick={() => setMenuOpen(false)}>
+            Movies
+          </a>
+        </li>
+
+        <li>
+          <a href="#services" onClick={() => setMenuOpen(false)}>
+            Services
+          </a>
+        </li>
+
+        <li>
+          <a href="#contact" onClick={() => setMenuOpen(false)}>
+            Contact
+          </a>
+        </li>
       </ul>
     </nav>
   );
